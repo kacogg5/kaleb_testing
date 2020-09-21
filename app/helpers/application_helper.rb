e88fa5f51f91c8c91727cf18e8ff0d69955df972
Nick Mark <nmgaultney@gmail.com>
2020-09-21 17:48:12 -0500

    Bootstrap

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+	def alert_class(flash_type)
+		case flash_type.to_sym
+			when :notice then "alert alert-info"
+	        when :success then "alert alert-success"
+	        when :error then "alert alert-danger"
+	        when :alert then "alert alert-danger"
+    	end
+	end
 end
