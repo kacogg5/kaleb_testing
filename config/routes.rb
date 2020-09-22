cbe739350d2112c7cd7f170162829450ac7adbbb
Capstone <dev@ubuntu.attlocal.net>
2020-09-22 00:02:22 +0000

    Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  get 'welcome/welcome'
+  root 'welcome#welcome'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
