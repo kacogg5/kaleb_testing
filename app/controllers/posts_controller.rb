0bde6f78500591758f30bbc98fb24275af3cf5fa
kaleb coggins <kalebjoshua.coggins@calbaptist.edu>
2020-09-25 19:46:30 -0700

    problem was an = sign

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,6 +8,7 @@ class PostsController < ApplicationController
 	end
 
 	def create
-		render plain: params[:post].inspect
+		# render plain: params[:post].inspect
+		@post = Post.new(params[:post])
 	end
 end
